92076062329ddc2c5f800e3bf20031c92b32f5ab
RAJESH KUMAR <RRAJAK915@GMAIL.COM>
2025-03-18 09:28:36 +0530

    Delete Feb-25 Assignments/Logic building Java Programing Assignment 2 directory

--- Feb-25 Assignments/Logic building Java Programing Assignment 2/NestedForDemo.java
+++
@@ -1,9 +0,0 @@
-class NestedForDemo{
-    public static void main(String args[]) {
-        for(int a=10; a>=5; a--) {
-            for(int b=10; b>=5; b--) {
-                System.out.print(a);
-            }
-        }
-    }
-}
